444bee8bc04f596653574b8625f02e6b2db86e53
Guillaume Massé <masgui@gmail.com>
2012-08-18 20:19:04 -0400

    blank lift project from template

---
+++ src/main/scala/net/liftweb/doc/snippet/HelloWorld.scala
@@ -0,0 +1,16 @@
+package net.liftweb.doc.snippet
+
+import net.liftweb.http.{S, SHtml}
+import net.liftweb.http.js.JsCmds
+import net.liftweb.util.Helpers._
+
+import xml.Text
+
+class HelloWorld {
+	def render = 
+		"#here *" #> Text("Hello World") &
+		"#click-me [onclick]" #> SHtml.ajaxInvoke{ () =>
+			println("Got it!")
+	      JsCmds.Alert("Got it!")
+	    }
+}
