4c69a6621ada58831da16452edb5809ad38b6e73
Guillaume Massé <guillaume.masse@polymtl.ca>
2012-09-01 19:24:01 -0400

    lift snippet type-selector transformation
    todo: find out how to use selection on value / multiple attributes
    using lift tag to avoid insertion of div element

--- src/main/scala/net/liftweb/doc/guide/snippet/transformation/builtin/TypeSelectors.scala
+++ src/main/scala/net/liftweb/doc/guide/snippet/transformation/builtin/TypeSelectors.scala
@@ -1,17 +1,20 @@
 package net.liftweb.doc.guide.snippet.transformation.builtin
 
+import net.liftweb.util.Helpers._
+
 object TypeSelectors
 {
-  /* Type Selection */
-  /*
-  @name
-  :button
-  :checkbox
-  :file
-  :password
-  :radio
-  :reset
-  :submit
-  :text
-  */
+  def render =
+  {
+    "@name [value]" #> "bob" &
+    ":button [value]" #> "bob" &
+    ":checkbox [checked]" #> "checked" &
+    ":file [class]" #> "hidden" &
+    ":password [placeholder]" #> "password" &
+    "value=hard [checked]" #> "" &  //:radio name=difficulty
+    "value=easy [checked]" #> "checked" & // :radio name=difficulty
+    ":text [value]" #> "a" &
+    ":reset [disabled]" #> "disabled" &
+    ":submit [disabled]" #> "disabled"
+  }
 }
